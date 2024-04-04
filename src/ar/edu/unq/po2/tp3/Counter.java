a61e219d1996002633a9103e94af2ab12037e569
Galarza Matias <matias.galarza.unq@gmail.com>
2024-04-03 23:34:51 -0300

    Terminado los Ejercicios 2 y 3 (TP3)

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -6,6 +6,10 @@ public class Counter {
 	
 	private ArrayList<Integer> counters = new ArrayList<Integer>();
 	
+	public ArrayList<Integer> getCounters() {
+		return counters;
+	}
+	
 	public void addNumber(int x) {
 		counters.add(x);
 	}
@@ -39,4 +43,110 @@ public class Counter {
 		}
 		return arrayOfMultiples.size();
 	}
+	
+	// EJERCICIO 2)
+	// EJEMPLO: getNumberWithMostEvenDigitsFromArray([10, 5, 40, 100, 224, 3148])	<< Debe retornar: 224
+	public int getNumberWithMostEvenDigitsFromArray(ArrayList<Integer> anArrayOfNumbers) {
+		
+		int currentNumber = anArrayOfNumbers.get(0);
+		ArrayList<Integer> currentArray = getNumberAsArray(currentNumber);
+		int biggestDigitsCounted = getAmountOfEvenOcurrencesFromArray(currentArray);
+		int numberToReturn = 0;
+		
+		if (getAmountOfEvenOcurrencesFromArray(currentArray) > 0) {
+			numberToReturn = anArrayOfNumbers.get(0);
+			biggestDigitsCounted = getAmountOfEvenOcurrencesFromArray(currentArray);
+		}
+		
+		
+		int index = 1;
+		while (index < anArrayOfNumbers.size()) {
+			currentNumber = anArrayOfNumbers.get(index);
+			currentArray = getNumberAsArray(currentNumber);
+			
+			if (getAmountOfEvenOcurrencesFromArray(currentArray) > biggestDigitsCounted) {
+				numberToReturn = anArrayOfNumbers.get(index);
+				biggestDigitsCounted = getAmountOfEvenOcurrencesFromArray(currentArray);
+			}
+			index += 1;
+		}
+		
+		return numberToReturn;
+	}
+	
+	public ArrayList<Integer> getNumberAsArray(int number) {
+		ArrayList<Integer> numberArray = new ArrayList<Integer>();
+		while (number > 0) {
+			int digit = number % 10;
+			number = number / 10;
+			numberArray.add(digit);
+		}
+		return numberArray;
+	}
+	
+	public int getAmountOfEvenOcurrencesFromArray(ArrayList<Integer> anArrayOfNumbers) {
+		ArrayList<Integer> arrayOfEvenNumbers = new ArrayList<Integer>();
+		for (int number:anArrayOfNumbers) {
+			if (number % 2 == 0) {
+				arrayOfEvenNumbers.add(number);
+			}
+		}
+		return arrayOfEvenNumbers.size();
+	}
+	
+	// EJERCICIO 3)
+	// EJEMPLO: getTheBiggestMultipleNumberBetween_and_(3,9)	<< Debe retornar: 999
+	public int getTheBiggestMultipleNumberBetween_and_(int x, int y) {
+		if (isTheNumber_multipleOf_(x,y) || isTheNumber_multipleOf_(y,x)) {
+			int biggestNumber = getBiggestNumberBetweenTwoNumbers(x,y);
+			ArrayList<Integer> currentMultipleArray = getAllMultiplesBetween0and1000from(biggestNumber);
+			return getBiggestNumberFrom(currentMultipleArray);
+		}
+		else {
+			return -1;
+		}
+	}
+
+	public boolean isTheNumber_multipleOf_(int number, int multiple) {
+		return multiple % number == 0;
+	}
+
+	public ArrayList<Integer> getAllMultiplesBetween0and1000from(int x) {
+		int indexMultiplier = 1;
+		int y = x;
+		ArrayList<Integer> currentMultipleArray = new ArrayList<Integer>();
+		while (y >= 0 && y <= 1000) {
+			currentMultipleArray.add(y);
+			indexMultiplier += 1;
+			y = x * indexMultiplier;
+		}
+		return currentMultipleArray;
+	}
+
+	public int getBiggestNumberFrom(ArrayList<Integer> sameMultiples) {
+		if (sameMultiples.isEmpty()) {
+			return -1;
+		}
+		else {
+			int index = 0;
+			int biggestNumber = sameMultiples.get(index);
+			while (index < sameMultiples.size()) {
+				biggestNumber = getBiggestNumberBetweenTwoNumbers(biggestNumber, sameMultiples.get(index));
+				index += 1;
+			}
+			return biggestNumber;
+		}
+	}
+
+	public int getBiggestNumberBetweenTwoNumbers(int x, int y) {
+		int biggestNumber;
+		if (x > y) {
+			biggestNumber = x;
+		}
+		else {
+			biggestNumber = y;
+		}
+		return biggestNumber;
+	}
+	
 }
--- test/ar/edu/unq/po2/tp3/CounterTestCase.java
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.po2.tp3;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,6 @@ public class CounterTestCase {
 	public void setUp() throws Exception {
 		// SetUp
 		counter = new Counter();
-
 		counter.addNumber(1);
 		counter.addNumber(3);
 		counter.addNumber(5);
@@ -53,5 +53,40 @@ public class CounterTestCase {
 		// Assert
 		assertEquals(amount1, 3);
 		assertEquals(amount2, 2);
+	}
+	
+	@Test
+	public void testDismantleNumbers() {
+		// SetUp (agrego nuevos counters para probar solo este test).
+		Counter counter2;
+		counter2 = new Counter();
+		counter2.addNumber(10);
+		counter2.addNumber(5);
+		counter2.addNumber(40);
+		counter2.addNumber(100);
+		counter2.addNumber(224);
+		counter2.addNumber(3148);
+		
+		Counter counter3 = new Counter();
+		counter3.addNumber(1);
+		
+		// Exercise
+		int amount = counter2.getNumberWithMostEvenDigitsFromArray(counter2.getCounters());
+		int amount2 = counter3.getNumberWithMostEvenDigitsFromArray(counter3.getCounters());
+		
+		// Assert
+		assertEquals(amount,224);
+		assertEquals(amount2,0);
+	}
+	
+	@Test
+	public void testEjercicio3( ) {
+		int number1 = counter.getTheBiggestMultipleNumberBetween_and_(3,9);
+		int number2 = counter.getTheBiggestMultipleNumberBetween_and_(100,2);
+		int number3 = counter.getTheBiggestMultipleNumberBetween_and_(1001,1);
+		
+		assertEquals(number1,999);
+		assertEquals(number2,1000);
+		assertEquals(number3,-1);
 	}
 }
