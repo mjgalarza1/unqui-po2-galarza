7ef5cf3f4693f55143e2f80ebd7899a4a6fa9fd1
Galarza Matias <matias.galarza.unq@gmail.com>
2024-05-09 02:40:04 -0300

    Terminado el PoquerStatus con Clase Carta

--- src/tp6/Poquer/PoquerStatus.java
+++ src/tp6/Poquer/PoquerStatus.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 
 public class PoquerStatus {
 
-	public String verificar(String c1, String c2, String c3, String c4, String c5) {
+	public String verificar(Carta c1, Carta c2, Carta c3, Carta c4, Carta c5) {
 		String resultado = "Nada";
-		List<String> manoDeCartas = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoDeCartas = List.of(c1,c2,c3,c4,c5);
 		
 		if (hayPoquerPara(manoDeCartas)) {
 			resultado = "Poquer";
@@ -20,36 +20,36 @@ public class PoquerStatus {
 		return resultado;
 	}
 
-	private boolean hayPoquerPara(List<String> manoDeCartas) {
+	private boolean hayPoquerPara(List<Carta> manoDeCartas) {
 		// El poquer se da cuando cuatro cartas poseen el mismo valor.
-		List<String> valores = valoresDeLasCartas(manoDeCartas);
-		Map<String, Long> ocurrencias = valores.stream()
+		List<Integer> valores = valoresDeLasCartas(manoDeCartas);
+		Map<Integer, Long> ocurrencias = valores.stream()
 				.collect(Collectors.groupingBy(valor -> valor, Collectors.counting()));
 		return (ocurrencias.values().stream().anyMatch(valor -> valor == 4));
 	}
 	
-	private boolean hayColorPara(List<String> manoDeCartas) {
+	private boolean hayColorPara(List<Carta> manoDeCartas) {
 		// El color se da cuando las cinco cartas son del mismo palo.
-		List<Character> palos = palosDeLasCartas(manoDeCartas);
-		char primerPalo = palos.get(0);
+		List<Palo> palos = palosDeLasCartas(manoDeCartas);
+		Palo primerPalo = palos.get(0);
 		return (palos.stream().allMatch(p -> p.equals(primerPalo)));
 	}
 
-	private boolean hayTrioPara(List<String> manoDeCartas) {
+	private boolean hayTrioPara(List<Carta> manoDeCartas) {
 		// El trio se da cuando tres cartas poseen el mismo valor.
-		List<String> valores = valoresDeLasCartas(manoDeCartas);
-		Map<String, Long> ocurrencias = valores.stream()
+		List<Integer> valores = valoresDeLasCartas(manoDeCartas);
+		Map<Integer, Long> ocurrencias = valores.stream()
 				.collect(Collectors.groupingBy(valor -> valor, Collectors.counting()));
 		return (ocurrencias.values().stream().anyMatch(valor -> valor == 3));
 	}
 
 	// MÉTODOS AUXILIARES
-	private List<String> valoresDeLasCartas(List<String> manoDeCartas) {
-		return manoDeCartas.stream().map(carta -> valorDeLaCarta(carta)).toList();
+	private List<Integer> valoresDeLasCartas(List<Carta> manoDeCartas) {
+		return manoDeCartas.stream().map(carta -> carta.getValor()).toList();
 	}
 	
-	private List<Character> palosDeLasCartas(List<String> manoDeCartas) {
-		return manoDeCartas.stream().map(carta -> paloDeLaCarta(carta)).toList();
+	private List<Palo> palosDeLasCartas(List<Carta> manoDeCartas) {
+		return manoDeCartas.stream().map(carta -> carta.getPalo()).toList();
 	}
 	
 	// Retorna el valor de la carta, es decir, sin el palo de la carta.
--- test/tp6/Poquer/PoquerStatusTest.java
+++ test/tp6/Poquer/PoquerStatusTest.java
@@ -9,6 +9,63 @@ public class PoquerStatusTest {
 
 	PoquerStatus poquer;
 	
+	// Atribuyo (de forma innecesaria) el mazo completo de cartas de Poker en variables:
+	Carta p2 = new Carta(Valor.DOS, Palo.PICA);
+	Carta p3 = new Carta(Valor.TRES, Palo.PICA);
+	Carta p4 = new Carta(Valor.CUATRO, Palo.PICA);
+	Carta p5 = new Carta(Valor.CINCO, Palo.PICA);
+	Carta p6 = new Carta(Valor.SEIS, Palo.PICA);
+	Carta p7 = new Carta(Valor.SIETE, Palo.PICA);
+	Carta p8 = new Carta(Valor.OCHO, Palo.PICA);
+	Carta p9 = new Carta(Valor.NUEVE, Palo.PICA);
+	Carta p10= new Carta(Valor.DIEZ, Palo.PICA);
+	Carta pJ = new Carta(Valor.J, Palo.PICA);
+	Carta pQ = new Carta(Valor.Q, Palo.PICA);
+	Carta pK = new Carta(Valor.K, Palo.PICA);
+	Carta pA = new Carta(Valor.A, Palo.PICA);
+		
+	Carta d2 = new Carta(Valor.DOS, Palo.DIAMANTE);
+	Carta d3 = new Carta(Valor.TRES, Palo.DIAMANTE);
+	Carta d4 = new Carta(Valor.CUATRO, Palo.DIAMANTE);
+	Carta d5 = new Carta(Valor.CINCO, Palo.DIAMANTE);
+	Carta d6 = new Carta(Valor.SEIS, Palo.DIAMANTE);
+	Carta d7 = new Carta(Valor.SIETE, Palo.DIAMANTE);
+	Carta d8 = new Carta(Valor.OCHO, Palo.DIAMANTE);
+	Carta d9 = new Carta(Valor.NUEVE, Palo.DIAMANTE);
+	Carta d10= new Carta(Valor.DIEZ, Palo.DIAMANTE);
+	Carta dJ = new Carta(Valor.J, Palo.DIAMANTE);
+	Carta dQ = new Carta(Valor.Q, Palo.DIAMANTE);
+	Carta dK = new Carta(Valor.K, Palo.DIAMANTE);
+	Carta dA = new Carta(Valor.A, Palo.DIAMANTE);
+		
+	Carta t2 = new Carta(Valor.DOS, Palo.TREBOL);
+	Carta t3 = new Carta(Valor.TRES, Palo.TREBOL);
+	Carta t4 = new Carta(Valor.CUATRO, Palo.TREBOL);
+	Carta t5 = new Carta(Valor.CINCO, Palo.TREBOL);
+	Carta t6 = new Carta(Valor.SEIS, Palo.TREBOL);
+	Carta t7 = new Carta(Valor.SIETE, Palo.TREBOL);
+	Carta t8 = new Carta(Valor.OCHO, Palo.TREBOL);
+	Carta t9 = new Carta(Valor.NUEVE, Palo.TREBOL);
+	Carta t10= new Carta(Valor.DIEZ, Palo.TREBOL);
+	Carta tJ = new Carta(Valor.J, Palo.TREBOL);
+	Carta tQ = new Carta(Valor.Q, Palo.TREBOL);
+	Carta tK = new Carta(Valor.K, Palo.TREBOL);
+	Carta tA = new Carta(Valor.A, Palo.TREBOL);
+		
+	Carta c2 = new Carta(Valor.DOS, Palo.CORAZON);
+	Carta c3 = new Carta(Valor.TRES, Palo.CORAZON);
+	Carta c4 = new Carta(Valor.CUATRO, Palo.CORAZON);
+	Carta c5 = new Carta(Valor.CINCO, Palo.CORAZON);
+	Carta c6 = new Carta(Valor.SEIS, Palo.CORAZON);
+	Carta c7 = new Carta(Valor.SIETE, Palo.CORAZON);
+	Carta c8 = new Carta(Valor.OCHO, Palo.CORAZON);
+	Carta c9 = new Carta(Valor.NUEVE, Palo.CORAZON);
+	Carta c10= new Carta(Valor.DIEZ, Palo.CORAZON);
+	Carta cJ = new Carta(Valor.J, Palo.CORAZON);
+	Carta cQ = new Carta(Valor.Q, Palo.CORAZON);
+	Carta cK = new Carta(Valor.K, Palo.CORAZON);
+	Carta cA = new Carta(Valor.A, Palo.CORAZON);
+	
 	@BeforeEach
 	public void setUp() {
 		// Setup
@@ -18,36 +75,36 @@ public class PoquerStatusTest {
 	@Test
 	public void testVerificarSiHayPoquer() {
 		// Assert (Verify)
-		assertEquals(poquer.verificar("2P", "QD", "2D", "2C", "2T"), "Poquer");
-		assertEquals(poquer.verificar("JT", "10P", "10D", "10C", "10T"), "Poquer");
-		assertEquals(poquer.verificar("10D", "QD", "QP", "QT", "QC"), "Poquer");
-		assertEquals(poquer.verificar("4T", "4D", "4P", "4C", "9T"), "Poquer");
-		assertEquals(poquer.verificar("AT", "AD", "AP", "AC", "QT"), "Poquer");
+		assertEquals(poquer.verificar(p2, dQ, d2, c2, t2), "Poquer");
+		assertEquals(poquer.verificar(tJ, p10, d10, c10, t10), "Poquer");
+		assertEquals(poquer.verificar(d10, dQ, pQ, tQ, cQ), "Poquer");
+		assertEquals(poquer.verificar(t4, d4, p4, c4, t9), "Poquer");
+		assertEquals(poquer.verificar(tA, dA, pA, cA, tQ), "Poquer");
 	}
 	
 	@Test
 	public void testVerificarSiHayColor() {
 		// Assert (Verify)
-		assertEquals(poquer.verificar("2P", "QP", "8P", "AP", "10P"), "Color");
-		assertEquals(poquer.verificar("JT", "10T", "KT", "5T", "AT"), "Color");
-		assertEquals(poquer.verificar("10C", "QC", "JC", "KC", "2C"), "Color");
-		assertEquals(poquer.verificar("4D", "9D", "AD", "2D", "QD"), "Color");
+		assertEquals(poquer.verificar(p2, pQ, p8, pA, p10), "Color");
+		assertEquals(poquer.verificar(tJ, t10, tK, t5, tA), "Color");
+		assertEquals(poquer.verificar(c10, cQ, cJ, cK, c2), "Color");
+		assertEquals(poquer.verificar(d4, d9, dA, d2, dQ), "Color");
 	}
 	
 	@Test
 	public void testVerificarSiHayTrio() {
 		// Assert (Verify)
-		assertEquals(poquer.verificar("2P", "QD", "2D", "2C", "JC"), "Trio");
-		assertEquals(poquer.verificar("JT", "10P", "10D", "8P", "10T"), "Trio");
-		assertEquals(poquer.verificar("7D", "1D", "QP", "QT", "QC"), "Trio");
-		assertEquals(poquer.verificar("AT", "AD", "AP", "5C", "8T"), "Trio");
+		assertEquals(poquer.verificar(p2, dQ, d2, c2, cJ), "Trio");
+		assertEquals(poquer.verificar(tJ, p10, d10, p8, t10), "Trio");
+		assertEquals(poquer.verificar(d7, dA, pQ, tQ, cQ), "Trio");
+		assertEquals(poquer.verificar(tA, dA, pA, c5, t8), "Trio");
 	}
 	
 	@Test
 	public void testVerificarQueNoHayNada() {
 		// Assert (Verify)
-		assertEquals(poquer.verificar("2P", "2D", "1T", "AP", "10C"), "Nada");
-		assertEquals(poquer.verificar("JC", "10T", "JD", "4C", "KP"), "Nada");
-		assertEquals(poquer.verificar("QD", "AP", "7T", "10D", "JD"), "Nada");
+		assertEquals(poquer.verificar(p2, d2, tA, pA, c10), "Nada");
+		assertEquals(poquer.verificar(cJ, t10, dJ, c4, pK), "Nada");
+		assertEquals(poquer.verificar(dQ, pA, t7, d10, dJ), "Nada");
 	}
 }
