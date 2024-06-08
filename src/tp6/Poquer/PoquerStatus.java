3535f7d63449b0fba824cee3f47dd6178abc2c87
Galarza Matias <matias.galarza.unq@gmail.com>
2024-06-07 21:34:13 -0300

    Terminado TP de poquer (Test Double)

--- src/tp6/Poquer/PoquerStatus.java
+++ src/tp6/Poquer/PoquerStatus.java
@@ -2,6 +2,7 @@ package tp6.Poquer;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class PoquerStatus {
@@ -50,6 +51,108 @@ public class PoquerStatus {
 	
 	private List<Palo> palosDeLasCartas(List<Carta> manoDeCartas) {
 		return manoDeCartas.stream().map(carta -> carta.getPalo()).toList();
+	}
+
+	public List<Carta> verificarGanadorEntreLasManos(List<Carta> mano1, List<Carta> mano2) {
+		int puntaje1 = puntajeDeMano(mano1);
+		int puntaje2 = puntajeDeMano(mano2);
+		List<Carta> ganador;
+		
+		if (puntaje1 > puntaje2) {
+			ganador = mano1;
+		} else if (puntaje1 < puntaje2) {
+			ganador = mano2;
+		} else {
+			ganador = verificarDesempateParaLasManos(mano1, mano2);
+		}
+		return ganador;
+	}
+
+	private String verificarParaMano(List<Carta> m) {
+		return verificar(m.get(0), m.get(1), m.get(2), m.get(3), m.get(4));
+	}
+
+	private List<Carta> verificarDesempateParaLasManos(List<Carta> mano1, List<Carta> mano2) {
+		String jugada1 = verificarParaMano(mano1);
+		String jugada2 = verificarParaMano(mano2);
+		List<Carta> ganador = mano1;
+		
+		if (jugada1 == "Poquer" && jugada2 == "Poquer") {
+			ganador = verificarGanadorDeJugadasPoquerOTrioParaLasManos(mano1, mano2, 4);
+		} else if (jugada1 == "Color" && jugada2 == "Color") {
+			ganador = verificarGanadorDeJugadasColorONadaParaLasManos(mano1, mano2);
+		} else if (jugada1 == "Trio" && jugada2 == "Trio") {
+			ganador = verificarGanadorDeJugadasPoquerOTrioParaLasManos(mano1, mano2, 3);
+		} else {
+			ganador = verificarGanadorDeJugadasColorONadaParaLasManos(mano1, mano2);
+		}
+		return ganador;
+	}
+
+	// ======================================================================================================
+	// SEGURO QUE ESTO ES MEJORABLE!!!
+	// ======================================================================================================
+	
+	private List<Carta> verificarGanadorDeJugadasColorONadaParaLasManos(List<Carta> mano1, List<Carta> mano2) {
+		List<Carta> ganador = mano1;
+		int puntajeJ1 = 0;
+		int puntajeJ2 = 0;
+		for(Carta c:mano1) {
+			puntajeJ1 += c.getValor();
+		}
+		for(Carta c:mano2) {
+			puntajeJ2 += c.getValor();
+		}
+		if (puntajeJ1 >= puntajeJ2) {
+			ganador = mano1;
+		} else {
+			ganador = mano2;
+		}
+		return ganador;
+	}
+
+	private List<Carta> verificarGanadorDeJugadasPoquerOTrioParaLasManos(List<Carta> mano1, List<Carta> mano2, int cantidad) {
+		List<Carta> ganador;
+		
+		int unaCartaDeLaJugada1 = valoresDeLasCartas(mano1).stream()
+			.filter(v -> ocurrencias(v, mano1, cantidad))
+			.findFirst()
+			.get();
+			
+		int unaCartaDeLaJugada2 = valoresDeLasCartas(mano2).stream()
+			.filter(v -> ocurrencias(v, mano2, cantidad))
+			.findFirst()
+			.get();
+			
+		unaCartaDeLaJugada1 *= cantidad;
+		unaCartaDeLaJugada2 *= cantidad;
+		
+		if (unaCartaDeLaJugada1 >= unaCartaDeLaJugada2) {
+			ganador = mano1;
+		} else {
+			ganador = mano2;
+		}
+		return ganador;
+	}
+
+	private boolean ocurrencias(int valor, List<Carta> mano, long cantidad) {
+		List<Integer> valores = valoresDeLasCartas(mano);
+		Map<Integer, Long> ocurrencias = valores.stream()
+				.collect(Collectors.groupingBy(v -> v, Collectors.counting()));
+		return ocurrencias.get(valor).equals(cantidad);
+	}
+
+	private int puntajeDeMano(List<Carta> mano) {
+		int resultado = 0;
+		
+		if (hayPoquerPara(mano)) {
+			resultado = 3;
+		} else if (hayColorPara(mano)) {
+			resultado = 2;
+		} else if (hayTrioPara(mano)) {
+			resultado = 1;
+		}
+		return resultado;
 	}
 
 }
--- test/tp6/Poquer/PoquerStatusMockitoTest.java
+++ test/tp6/Poquer/PoquerStatusMockitoTest.java
@@ -6,6 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
+
+/*Jugadas de Póquer
+Nuevamente, es necesario agregar funcionalidad para que las jugadas puedan compararse y saber cual puede ser el
+ganador de una mano. El orden de importancia entre jugadas es el siguiente: Póquer le gana a Color y Color le gana a
+Trio. La relación es transitiva. Por su parte, si las jugadas son iguales se desempata por el valor de las cartas que la
+componen. El orden de las cartas es el siguiente: A, K, Q, J, 10, ..., 2. En orden de importancia descendente. Es decir,
+un trio con tres ases le gana a un trio con tres K.
+. 1) Identifique los casos a testear.
+. 2) Defina los test para el modelado de jugadas. Recuerde seguir la metodología TDD y la POO!.
+. 3) Identifique en sus tests los setup, excercise, verify, teardown y doc. ¿Qué tipo de Test Doubles usó?
+. 4) Actualice los test programados anteriormente para que el verificar ahora retorne instancias de jugadas*/
+
 public class PoquerStatusMockitoTest {
 
 	PoquerStatus poquer;
@@ -133,6 +146,300 @@ public class PoquerStatusMockitoTest {
 		verify(c4).getPalo();
 		verify(c5).getPalo();
 		assertEquals(poquer.verificar(c1, c2, c3, c4, c5), "Nada");
+	}
+	
+	@Test
+	public void testVerificarGanadorConTrio() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Trio
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(7);
+		when(c3.getValor()).thenReturn(7);
+		when(c4.getValor()).thenReturn(7);
+		when(c5.getValor()).thenReturn(6);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.CORAZON);
+		when(c3.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c4.getPalo()).thenReturn(Palo.TREBOL);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Nada
+		when(c6.getValor()).thenReturn(5);
+		when(c7.getValor()).thenReturn(4);
+		when(c8.getValor()).thenReturn(2);
+		when(c9.getValor()).thenReturn(9);
+		when(c10.getValor()).thenReturn(3);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.CORAZON);
+		when(c8.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c9.getPalo()).thenReturn(Palo.TREBOL);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador1);
+	}
+	
+	@Test
+	public void testVerificarDesempateConTrio() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Trio
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(7);
+		when(c3.getValor()).thenReturn(7);
+		when(c4.getValor()).thenReturn(7);
+		when(c5.getValor()).thenReturn(6);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.CORAZON);
+		when(c3.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c4.getPalo()).thenReturn(Palo.TREBOL);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Trio
+		when(c6.getValor()).thenReturn(2);
+		when(c7.getValor()).thenReturn(10);
+		when(c8.getValor()).thenReturn(8);
+		when(c9.getValor()).thenReturn(8);
+		when(c10.getValor()).thenReturn(8);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.CORAZON);
+		when(c8.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c9.getPalo()).thenReturn(Palo.TREBOL);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador2);
+	}
+	
+	@Test
+	public void testVerificarGanadorConColor() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Color
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(8);
+		when(c3.getValor()).thenReturn(4);
+		when(c4.getValor()).thenReturn(2);
+		when(c5.getValor()).thenReturn(6);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.PICA);
+		when(c3.getPalo()).thenReturn(Palo.PICA);
+		when(c4.getPalo()).thenReturn(Palo.PICA);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Trio
+		when(c6.getValor()).thenReturn(5);
+		when(c7.getValor()).thenReturn(7);
+		when(c8.getValor()).thenReturn(7);
+		when(c9.getValor()).thenReturn(3);
+		when(c10.getValor()).thenReturn(7);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.CORAZON);
+		when(c8.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c9.getPalo()).thenReturn(Palo.TREBOL);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador1);
+	}
+	
+	@Test
+	public void testVerificarDesempateConColor() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Color
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(8);
+		when(c3.getValor()).thenReturn(4);
+		when(c4.getValor()).thenReturn(2);
+		when(c5.getValor()).thenReturn(6);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.PICA);
+		when(c3.getPalo()).thenReturn(Palo.PICA);
+		when(c4.getPalo()).thenReturn(Palo.PICA);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Trio
+		when(c6.getValor()).thenReturn(9);
+		when(c7.getValor()).thenReturn(8);
+		when(c8.getValor()).thenReturn(4);
+		when(c9.getValor()).thenReturn(9);
+		when(c10.getValor()).thenReturn(6);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.PICA);
+		when(c8.getPalo()).thenReturn(Palo.PICA);
+		when(c9.getPalo()).thenReturn(Palo.PICA);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador2);
+	}
+	
+	@Test
+	public void testVerificarGanadorConPoquer() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Trio
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(7);
+		when(c3.getValor()).thenReturn(7);
+		when(c4.getValor()).thenReturn(7);
+		when(c5.getValor()).thenReturn(7);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.CORAZON);
+		when(c3.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c4.getPalo()).thenReturn(Palo.TREBOL);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Nada
+		when(c6.getValor()).thenReturn(5);
+		when(c7.getValor()).thenReturn(4);
+		when(c8.getValor()).thenReturn(2);
+		when(c9.getValor()).thenReturn(9);
+		when(c10.getValor()).thenReturn(3);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.PICA);
+		when(c8.getPalo()).thenReturn(Palo.PICA);
+		when(c9.getPalo()).thenReturn(Palo.PICA);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador1);
+	}
+	
+	@Test
+	public void testVerificarDesempateConPoquer() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Trio
+		when(c1.getValor()).thenReturn(9);
+		when(c2.getValor()).thenReturn(7);
+		when(c3.getValor()).thenReturn(7);
+		when(c4.getValor()).thenReturn(7);
+		when(c5.getValor()).thenReturn(7);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.CORAZON);
+		when(c3.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c4.getPalo()).thenReturn(Palo.TREBOL);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Trio
+		when(c6.getValor()).thenReturn(8);
+		when(c7.getValor()).thenReturn(8);
+		when(c8.getValor()).thenReturn(8);
+		when(c9.getValor()).thenReturn(8);
+		when(c10.getValor()).thenReturn(13);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.CORAZON);
+		when(c8.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c9.getPalo()).thenReturn(Palo.TREBOL);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador2);
+	}
+	
+	@Test
+	public void testVerificarDesempateConNada() {
+		// Setup (para el segundo jugador)
+		Carta c6 = mock(Carta.class);
+		Carta c7 = mock(Carta.class);
+		Carta c8 = mock(Carta.class);
+		Carta c9 = mock(Carta.class);
+		Carta c10 = mock(Carta.class);
+		
+		// JUGADOR 1: Trio
+		when(c1.getValor()).thenReturn(2);
+		when(c2.getValor()).thenReturn(3);
+		when(c3.getValor()).thenReturn(4);
+		when(c4.getValor()).thenReturn(5);
+		when(c5.getValor()).thenReturn(6);
+		
+		when(c1.getPalo()).thenReturn(Palo.PICA);
+		when(c2.getPalo()).thenReturn(Palo.CORAZON);
+		when(c3.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c4.getPalo()).thenReturn(Palo.TREBOL);
+		when(c5.getPalo()).thenReturn(Palo.PICA);
+		
+		// JUGADOR 2: Nada
+		when(c6.getValor()).thenReturn(7);
+		when(c7.getValor()).thenReturn(8);
+		when(c8.getValor()).thenReturn(9);
+		when(c9.getValor()).thenReturn(10);
+		when(c10.getValor()).thenReturn(11);
+		
+		when(c6.getPalo()).thenReturn(Palo.PICA);
+		when(c7.getPalo()).thenReturn(Palo.CORAZON);
+		when(c8.getPalo()).thenReturn(Palo.DIAMANTE);
+		when(c9.getPalo()).thenReturn(Palo.TREBOL);
+		when(c10.getPalo()).thenReturn(Palo.PICA);
+		
+		List<Carta> manoJugador1 = List.of(c1,c2,c3,c4,c5);
+		List<Carta> manoJugador2 = List.of(c6,c7,c8,c9,c10);
+		
+		// Verify
+		assertEquals(poquer.verificarGanadorEntreLasManos(manoJugador1, manoJugador2), manoJugador2);
 	}
 	
 }
